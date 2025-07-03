889d89cfae94895e9a6a912ac12c9d2b2339e984
Ju1iaG <yuliya.galkina.04@inbox.ru>
2025-07-03 15:11:20 +0400

    Added full name check

--- HospitalApp/hospital-crud/src/main/java/com/example/hospital_crud/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.hospital_crud.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
